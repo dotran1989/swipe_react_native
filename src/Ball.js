66b3a470df288152f9ebdba406d9a4ea255df9c6
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-16 15:12:13 +0700

    animate ball

--- src/Ball.js
+++ src/Ball.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import { View, Animated } from 'react-native';
 
 class Ball extends Component{
+    componentWillMount() {
+        this.position = new Animated.ValueXY({ x: 0, y: 0 }); // ({ x: 0, y: 0 })
+        /* 
+        this.position + this.position.getLayout()
+        Object {
+            "left": 0,
+            "top": 0 
+        }*/
+        Animated.spring(this.position, {
+            toValue: { x: 200, y: 500 }
+        }).start();
+    }
+
     render() {
-        return (
-            <View style={styles.ball}/>
+        return ( // getLayout() contains some information to tell the animated view how it should be changing.
+            <Animated.View style={this.position.getLayout()}>
+                <View style={styles.ball}/>
+            </Animated.View>
         );
     }
 }
