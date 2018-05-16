ad6b24c9a69897ca561488b90c13f5c8b8ec80b1
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-16 15:37:46 +0700

    touchable to animate

--- src/Ball.js
+++ src/Ball.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, TouchableOpacity, Text } from 'react-native';
 
 class Ball extends Component{
+    state = {
+        text: 'hi',
+        color: 'blue'
+    };
+
     componentWillMount() {
+        // console.log(`this: ${JSON.stringify(this)}`); // typeError: converting circular structure for JSON
         this.position = new Animated.ValueXY({ x: 0, y: 0 }); // ({ x: 0, y: 0 })
+        console.log(`this.position: ${JSON.stringify(this.position)}`);
         /* 
         this.position + this.position.getLayout()
         Object {
             "left": 0,
             "top": 0 
         }*/
-        Animated.spring(this.position, {
-            toValue: { x: 200, y: 500 }
-        }).start();
+    }
+
+    _onPress() {
+        setTimeout(() => {
+            Animated.spring(this.position, {
+                toValue: { x: 200, y: 500 }
+            }).start();
+            this.setState({ text: 'animated', color: 'red' })
+        }, 75);
     }
 
     render() {
         return ( // getLayout() contains some information to tell the animated view how it should be changing.
-            <Animated.View style={this.position.getLayout()}>
-                <View style={styles.ball}/>
-            </Animated.View>
+            <View>
+                <Animated.View style={[this.position.getLayout(), {borderColor: this.state.color, borderWidth: 1}]}>
+                    <View style={styles.ball}/>
+                </Animated.View>
+                <TouchableOpacity onPress={this._onPress.bind(this)}>
+                    <Text>Press Me!</Text>
+                    <Text>Text: {this.state.text}</Text>
+                </TouchableOpacity>
+            </View>
         );
     }
 }
@@ -30,7 +49,7 @@ const styles = {
         width: 60,
         borderRadius: 30,
         borderWidth: 30,
-        borderColor: 'black'
+        borderColor: 'blue'
     }
 }
 
