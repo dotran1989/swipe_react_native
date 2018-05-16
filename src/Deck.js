36a6e8066291edcb20d40fbcf4d0874452713f1c
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-16 16:14:02 +0700

    render item

---
+++ src/Deck.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Animated, TouchableOpacity, Text } from 'react-native';
+
+class Deck extends Component {
+    renderCards() {
+        return this.props.data.map(item => {
+            // return this.props.renderCard(item);
+            return (
+                <Text>{item.text}</Text>
+            )
+        });
+    };
+
+    render() {
+        // 'this.renderCards()' only need to run once and immediately when the component renders. Helper methods use parenthesis.
+        return (
+            <View>
+                {this.renderCards()}
+            </View>
+        );
+    }
+};
+
+export default Deck;
